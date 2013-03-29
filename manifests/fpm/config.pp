ee7a0d34cf453bb26b01bb55c3e9cd0b9a94cbdc
Leon Bogaert <leon@tim-online.nl>
2013-03-30 00:08:41 +0100

    Install before packages dependent on php

--- manifests/fpm/config.pp
+++ manifests/fpm/config.pp
@@ -1,12 +1,17 @@
 class php::fpm::config {
 
+		# do package before config
+		Class['php::fpm::package'] -> Class['php::fpm::config']
+
+		# notify service class
+		Class['php::fpm::config'] ~> Class['php::fpm::service']
+
 		file { '/etc/php5/fpm/conf.d/custom.ini':
 			source  => "puppet:///modules/php/custom.ini",
 			ensure  => $php::fpm::ensure,
 			owner   => 'root',
 			group   => 'root',
 			mode    => '0644',
-			require => Class['php::fpm::package']
 		}
 
 }
--- manifests/fpm/package.pp
+++ manifests/fpm/package.pp
@@ -1,6 +1,9 @@
 class php::fpm::package {
 
 	package  { $php::fpm::package_name:
-		ensure => present
+		ensure => present,
+		before => Class['php'], # make phpmyadmin not install `libapache2-mod-php5`
 	}
+
+	class { 'php': } # virtual class doesn't work at the moment (http://projects.puppetlabs.com/issues/12231)
 }
