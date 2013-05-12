0c4093c03422574212bdcd971e3a083a3f1b406b
Leon Bogaert <leon@tim-online.nl>
2013-05-12 12:12:46 +0200

    Added default dependencies

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,7 +17,7 @@ class php::params {
 	### Application related parameters
 
 	$packages = $::operatingsystem ? {
-		default => ['php5-cli']
+		default => ['php5-cli', 'php5-gd', 'php5-curl']
 	}
 
 	$enabled = true
